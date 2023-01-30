ff480e0c1034ac35180d36056f497783cada52c2
shixinzia <111138018+shixinzia@users.noreply.github.com>
2023-01-30 23:05:18 +0800

    Update NetworkDirectional.java

--- src/main/java/io/github/sefiraat/networks/slimefun/network/NetworkDirectional.java
+++ src/main/java/io/github/sefiraat/networks/slimefun/network/NetworkDirectional.java
@@ -118,7 +118,7 @@ public abstract class NetworkDirectional extends NetworkObject {
 
         for (BlockFace blockFace : VALID_FACES) {
             final Block block = blockMenu.getBlock().getRelative(blockFace);
-            final SlimefunItem slimefunItem = block.getType();
+            final Material slimefunItem = block.getType();
             if (slimefunItem != null) {
                 switch (blockFace) {
                     case NORTH -> blockMenu.replaceExistingItem(getNorthSlot(), getDirectionalSlotPane(blockFace, slimefunItem, blockFace == direction));
