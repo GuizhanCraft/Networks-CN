398bef7e2bfa87d1c511987c76b4c7ca3a328725
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2025-02-22 12:04:47 -0300

    inline brackets and identation pattern

--- src/main/java/io/github/sefiraat/networks/events/NetworkCraftEvent.java
+++ src/main/java/io/github/sefiraat/networks/events/NetworkCraftEvent.java
@@ -11,23 +11,21 @@ import org.bukkit.inventory.ItemStack;
 
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 
-public class NetworkCraftEvent extends PlayerEvent implements Cancellable
-{
-	private static final HandlerList handlers = new HandlerList();
+public class NetworkCraftEvent extends PlayerEvent implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
 
-	private final SlimefunItem machine;
-	private final ItemStack[] input;
+    private final SlimefunItem machine;
+    private final ItemStack[] input;
     private ItemStack output;
     private boolean cancelled;
 
-	public NetworkCraftEvent(Player p, SlimefunItem machine, ItemStack[] input, ItemStack output) 
-	{
-		super(p);
-		
-		this.input = input;
+    public NetworkCraftEvent(Player p, SlimefunItem machine, ItemStack[] input, ItemStack output) {
+        super(p);
+        
+        this.input = input;
         this.output = output;
-		this.machine = machine;
-	}
+        this.machine = machine;
+    }
 
     public @Nonnull ItemStack[] getInput() {
         return this.input;
@@ -37,33 +35,32 @@ public class NetworkCraftEvent extends PlayerEvent implements Cancellable
         return this.output;
     }
 
-	public @Nullable ItemStack setOutput(@Nullable ItemStack output) {
+    public @Nullable ItemStack setOutput(@Nullable ItemStack output) {
         ItemStack oldOutput = this.output;
         this.output = output;
         return oldOutput;
     }
 
-	public SlimefunItem getMachine()
-	{
-		return this.machine;
-	}
+    public SlimefunItem getMachine() {
+        return this.machine;
+    }
 
-	@Override
-	public boolean isCancelled() {
-		return this.cancelled;
-	}
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
 
-	@Override
-	public void setCancelled(boolean cancel) {
-		this.cancelled = cancel;
-	}
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
 
-	public static @Nonnull HandlerList getHandlerList() {
+    public static @Nonnull HandlerList getHandlerList() {
         return handlers;
     }
 
-	@Override
-	public HandlerList getHandlers() {
-		return getHandlerList();
-	}
+    @Override
+    public HandlerList getHandlers() {
+        return getHandlerList();
+    }
 }
